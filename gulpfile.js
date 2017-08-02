77bed7ff4ffc954fe1d89f9f17f4a04c5e9e9718
Rafael Franco <rafaelfr@outlook.com>
2017-08-02 20:46:17 -0300

    Added .gitignore

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+	' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+	' * Copyright ' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+	' */\n',
+	''
+].join('');
+
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function () {
+	return gulp.src('scss/playground.scss')
+		.pipe(sass())
+		.pipe(header(banner, {
+			pkg: pkg
+		}))
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function () {
+	return gulp.src('css/playground.css')
+		.pipe(cleanCSS({
+			compatibility: 'ie8'
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
+
+// Minify custom JS
+gulp.task('minify-js', function () {
+	return gulp.src('js/playground.js')
+		.pipe(uglify({mangle: { except: ['topFunction'] }}))
+		.pipe(header(banner, {
+			pkg: pkg
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('js'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
+
+// Default task
+gulp.task('default', ['sass', 'minify-css', 'minify-js']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function () {
+	browserSync.init({
+		server: {
+			baseDir: ''
+		},
+	})
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function () {
+	gulp.watch('scss/*.scss', ['sass']);
+	gulp.watch('css/*.css', ['minify-css']);
+	gulp.watch('js/*.js', ['minify-js']);
+	// Reloads the browser whenever HTML or JS files change
+	gulp.watch('*.html', browserSync.reload);
+	gulp.watch('js/**/*.js', browserSync.reload);
+});
